87ba653baec1871998f0518b1c85fa97500b01fd
joseluisbugallo <uo244702@uniovi.es>
2019-03-06 13:43:37 +0100

    Friends List

---
+++ src/app/friends/friends.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { RdfService } from '../services/rdf.service';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.css']
+})
+export class FriendsComponent implements OnInit {
+
+  constructor(private rdf: RdfService) { }
+
+  ngOnInit() {
+    this.loadFriends();
+  }
+
+  async loadFriends() {
+    try {
+      const list_friends = await this.rdf.getFriends();
+      if (list_friends) {
+        for (let i = 0; i < list_friends.length; i++) {
+          return list_friends[i];
+        }
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
+}
