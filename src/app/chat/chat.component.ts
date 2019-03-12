70686ae42c50f6200af58658cd8821e7050d106b
joseluisbugallo <uo244702@uniovi.es>
2019-03-12 23:39:13 +0100

    Creacion de carpeta en el pod

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -3,7 +3,6 @@ import { RdfService } from '../services/rdf.service';
 import { ActivatedRoute, Params } from '@angular/router';
 
 
-import solid_file_client from 'solid-file-client';
 
 @Component({
     selector: 'app-chat',
@@ -16,6 +15,7 @@ export class ChatComponent implements OnInit {
 
     fileClient: any;
     ruta_seleccionada: string;
+    htmlToAdd : string;
 
     constructor(private rdf: RdfService, private rutaActiva: ActivatedRoute) {
         this.rutaActiva.params.subscribe(data => {
@@ -27,7 +27,7 @@ export class ChatComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.fileClient = solid_file_client;
+        this.fileClient = require('solid-file-client');
     }
 
     private getUserByUrl(ruta: string): string {
@@ -51,15 +51,14 @@ export class ChatComponent implements OnInit {
         //Necesito logearme en el cliente para que me de permiso, sino me dara un error al intentar
         //crear la carpeta. Como ya estoy en sesion no abre nada pero si se abre la consola se ve
         // que se ejecuta correctamente.
-        this.fileClient.popupLogin().then(webId => {
-            console.log(`Logged in as ${webId}.`);
-        }, err => console.log(err));
+
 
         //Le paso la URL de la carpeta y se crea en el pod. SI ya esta creada no se si la sustituye o no hace nada
         this.fileClient.createFolder(solidId).then(success => {
             console.log(`Created folder ${solidId}.`);
+            this.htmlToAdd = '<div>Carpeta Creada! Ve a tu pod para comprobarlo</div>';
         }, err => console.log(err));
 
-        document.write('Carpeta creada. Mira en tu POD para verla!');
+        this.htmlToAdd = '<div>Carpeta Creada! Ve a tu pod para comprobarlo</div>';
     }
 }
