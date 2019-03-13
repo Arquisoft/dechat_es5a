7ef1f2af80bf1e103810c09774aa5f2e1556f24f
Carlos Gómez <charlygomezcolmenero@gmail.com>
2019-03-13 15:52:35 +0100

    Added logout functionality

--- src/app/fragments/navbar/navbar.component.ts
+++ src/app/fragments/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../../services/solid.auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
   }
 
-
+  logout(){
+    console.log("logout");
+    this.auth.solidSignOut();
+  }
 
 }
